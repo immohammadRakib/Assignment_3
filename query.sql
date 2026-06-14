3d766ca1282c2d1bcf0fe1c593f55e3f456eab89
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-06-14 19:31:33 +0600

    twelve-commit

--- query.sql
+++ query.sql
@@ -22,12 +22,11 @@ CREATE TABLE Matches (
         fixture VARCHAR(150) NOT NULL,
         tournament_category VARCHAR(100) NOT NULL,
         base_ticket_price DECIMAL(10, 2) NOT NULL,
-        match_status VARCHAR(50) NOT NULL,
+        match_status match_status_type NOT NULL,
 
 -- Constraints
 CONSTRAINT pk_matches PRIMARY KEY (match_id),
-        CONSTRAINT chk_matches_price CHECK (base_ticket_price >= 0),
-        CONSTRAINT chk_matches_status CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+        CONSTRAINT chk_matches_price CHECK (base_ticket_price >= 0)
     );
 
 CREATE TABLE Bookings (
